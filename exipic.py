338a9f98e5021fc7e4601357a2e093d68e7e67dc
Hella Breitkopf <hella.breitkopf@gmail.com>
2019-03-14 13:49:33 +0100

    format exif values

--- exipic.py
+++ exipic.py
@@ -13,19 +13,25 @@ status: testing Pillow / early development
 import os
 # PIL from Pillow
 import PIL
-#from PIL import Image
-#from PIL.ExifTags import TAGS
+import PIL.Image
+import PIL.ExifTags
 
+# which symbol should be used instead of the decimal delimiter '.' e.g. for aperture (blende)
+# since a dot is not good in file names we use something else
+DELIMITER = '-'
+
+# if the lens is analog, the value for aperture or length might be zero
+# which string should be written instead?
+NOVALUE = 'x'
 
 def print_exif(img):
     """print exif data
     this is a temporary function
     to explore how to create a usefull filename"""
-
-    # https://stackoverflow.com/a/4765242
+    # fetch tagging from https://stackoverflow.com/a/4765242
     exif = {
         PIL.ExifTags.TAGS[k]: v
-        for k, v in img._getexif().items()
+        for k, v in img._getexif().items() # pylint: disable=protected-access
         if k in PIL.ExifTags.TAGS
     }
 
@@ -33,29 +39,79 @@ def print_exif(img):
 
     # TODO date + time formating (YYYYmmdd-HHMMSS)
 
-    # TODO fnumber aka aperture (blende)
-    # * if the fnmuber is missing (analog lense) you get 0.0 ...
-    # * is there a possibility for long floats? -> watch output,
-    # it might be better to format output after the decimal point
-    blende = exif['FNumber'][0] / exif['FNumber'][1]
+    aperture = format_aperture(exif['FNumber'])
 
+    _exposure_time = format_exposuretime_tuple(exif['ExposureTime'])
+    _focal_len = format_focal_length_tuple(exif['FocalLength'])
 
-    # TODO exposure time (belichtungszeit)
-    # I've seen following things in the PIL tuple:
-    # 6, 1  -> 6s
-    # 1, 125 -> 1/125 s
-    # 10, 12500 -> 1/1250 s
-    belichtungszeit1 = f"{exif['ExposureTime'][0]}/{exif['ExposureTime'][1]}"
-    belichtungszeit2 = exif['ExposureTime'][0] / exif['ExposureTime'][1]
-
-    print(f"date: {exif['DateTimeOriginal']}\n" #foo
-          f"kamera: {exif['Model']}\n"
-          f"brennweite: {exif['FocalLength']}\n"
-          f"belichtungszeit: {belichtungszeit1}\n"
-          f"belichtungszeit: {belichtungszeit2}\n"
-          f"blende: {blende}\n"
+    print(f"date: {exif['DateTimeOriginal']}\n"
+          f"camera: {exif['Model']}\n"
+          f"Focal Length: {_focal_len}\n"
+          f"Exposure Time: {_exposure_time}\n"
+          f"Aperture: {aperture}\n"
           )
 
+def format_aperture(_tuple):
+    "format aperture tuple to short printable string"
+    zaehler = _tuple[0]
+    nenner = _tuple[1]
+
+    if zaehler == 0:
+        return NOVALUE
+
+    if zaehler % nenner == 0:
+        return zaehler
+
+    return str(zaehler/nenner).replace('.', DELIMITER)
+
+def format_focal_length_tuple(_tuple):
+    """format FocalLenght tuple to short printable string
+    we ignore the position after the decimal point
+    because it is usually not very essential for focal length
+    """
+    zaehler = _tuple[0]
+    nenner = _tuple[1]
+
+    if zaehler == 0:
+        return NOVALUE
+
+    if zaehler % 10 == 0 and nenner % 10 == 0:
+        # example: change 110/10  -> 11
+        zaehler = zaehler // 10
+        nenner = nenner // 10
+
+    if nenner == 1:
+        # example: change 8/1 to 8mm
+        _string = f"{zaehler}mm"
+    else:
+        # example: 524/10 -> 52mm
+        # we ignore the position after the decimal point
+        # because it is usually not very essential for focal length
+        _string = f"{zaehler//nenner}mm"
+    return _string
+
+def format_exposuretime_tuple(_tuple):
+    """format ExposureTime tuple to short printable string
+    fractions over or equal 1 second are marked with s, e.g. 8s
+    fractions below 1 second are broken down to the divisor,
+    this is a bit incorrect but short and common e.g. in cameras
+    (and we want to have a short string)
+    """
+    zaehler = _tuple[0]
+    nenner = _tuple[1]
+    if zaehler % 10 == 0 and nenner % 10 == 0:
+        # change 10/1250 to 1/125
+        zaehler = zaehler // 10
+        nenner = nenner // 10
+
+    if nenner == 1:
+        # change 6/1 -> 6s
+        # fractions => 1s with s for seconds
+        _string = f"{zaehler}s"
+    else:
+        # change 1/125 -> 125
+        _string = f"{nenner}"
+    return _string
 
 if __name__ == '__main__':
 
